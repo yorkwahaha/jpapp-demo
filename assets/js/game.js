f4207da35fd987e506d7863eeb380ac5dd7352ae
York <yorkwahaha@gmail.com>
2026-05-07 21:55:35 +0800

    chore: bump version to 26050702

--- assets/js/game.js
+++ assets/js/game.js
@@ -933,7 +933,7 @@ const _jpApp = Vue.createApp({
         };
 
         // ---- [ CONSTANTS & SETTINGS ] ----
-        const APP_VERSION = window.APP_VERSION || "26050701";
+        const APP_VERSION = window.APP_VERSION || "26050702";
 
         const appVersion = ref(APP_VERSION);
         const changelogManager = window.JPAPPChangelogManager || {
